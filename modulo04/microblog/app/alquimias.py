bf1a45db50fa06d8d3c2ffd27ef8522b6d1c9755
Matheus Bueno <matheussbueno10@hotmail.com>
2026-05-30 16:07:29 -0300

    Delete modulo04 directory

--- modulo04/microblog/app/alquimias.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Instância do SQLAlchemy isolada em seu próprio arquivo ("alquimias" = alchemy)
-# para evitar importações circulares entre __init__.py, routes.py e models.py.
-db = SQLAlchemy()
